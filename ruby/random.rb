51848dd613e4f146a0c6450427896d267e89a6c8
takuzo <takuya.koiso.s@gmail.com>
2023-11-12 06:58:13 +0900

    clean 右代入を使用

--- ruby/random.rb
+++ ruby/random.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
-group_a, group_b =
-  'ABCDEF' # メンバを定義
+'ABCDEF' # メンバを定義
   .chars # 配列化
   .shuffle # シャッフル
   .each_slice(rand(3..4)) # ランダムに分解=要素数3or4
   .to_a # Enumarable型を配列に変換
   .map(&:sort) # メンバをアルファベット順にソート
-  .sort_by(&:size) # グループをサイズ小さい順にソート
+  .sort_by(&:size) => [group_a, group_b ] # グループをサイズ小さい順にソートして代入
 
 p group_a
 p group_b
