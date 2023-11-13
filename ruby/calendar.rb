c4344405a784ad376ebdcdd3b51d9ac5f0d2e8af
takuzo <takuya.koiso.s@gmail.com>
2023-11-13 14:55:41 +0900

    clean date処理(レビュー事項)

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -13,13 +13,13 @@ if month && !(1..12).include?(month)
 end
 
 # 年月のデフォルトは月初。オプションが優先
-month ||= Date.today.month
-year = Date.today.year
+today = Date.today
+month ||= today.month
+year = today.year
 
 # Dateクラスから開始曜日、開始日、終了日を変数に代入
 start_date = Date.new(year, month, 1)
-start_day_of_week = start_date.wday # 日曜が0、土曜が6
-start_day_of_week = 7 if start_date.sunday? # 今後の処理のために日曜日を7として扱う
+start_day_of_week = start_date.sunday? ? 7 : start_date.wday # 日曜日を7として扱い、月〜土 = 1~6
 start_day = start_date.day
 end_day = Date.new(year, month, -1).day
 
