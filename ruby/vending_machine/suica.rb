ebd356539a254698838761010da6fbee9d0c9164
takuzo <takuya.koiso.s@gmail.com>
2023-11-14 08:36:38 +0900

    step1

---
+++ ruby/vending_machine/suica.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Suica
+  def initialize
+    # 残高
+    @balance = 500
+  end
+
+  def balance
+    @balance
+  end
+
+  def charge(deposit)
+    raise '入金は100円以上からです' if deposit < 100
+
+    @balance += deposit
+  end
+end
---
+++ ruby/vending_machine/suica_test.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative 'suica'
+
+class SuicaTest < Minitest::Test
+  def setup
+    @suica = Suica.new
+  end
+
+  def test_default_500_yen
+    assert_equal 500, @suica.balance
+  end
+
+  def test_charge
+    @suica.charge(100)
+    assert_equal 600, @suica.balance
+  end
+
+  def test_charge_less_than_100_raise
+    e = assert_raises(RuntimeError) do
+      @suica.charge(99)
+    end
+    assert_equal '入金は100円以上からです', e.message
+  end
+end
