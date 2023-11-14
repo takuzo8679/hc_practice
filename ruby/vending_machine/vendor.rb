6e42c15c19952b788f8241fb34136981720ec225
takuzo <takuya.koiso.s@gmail.com>
2023-11-14 10:31:32 +0900

    step3 add buy process

--- ruby/vending_machine/vendor.rb
+++ ruby/vending_machine/vendor.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'juice'
+require_relative 'suica'
 
 INITIAL_STOCK = [
   { name: 'pepci', price: 150, num: 5 }
@@ -8,6 +9,7 @@ INITIAL_STOCK = [
 
 class Vendor
   def initialize
+    @sales = 0
     @stock = []
     INITIAL_STOCK.map do |juice|
       juice[:num].times do
@@ -18,5 +20,20 @@ class Vendor
 
   def stock
     @stock.size
+  end
+
+  def can_buy?(suica)
+    return false if @stock.empty?
+    return false if suica.balance < @stock[0].price
+
+    true
+  end
+
+  def buy(suica)
+    raise 'チャージ残高か在庫が不足しています' unless can_buy?(suica)
+
+    juice = @stock.pop
+    @sales += juice.price
+    suica.withdrawal(juice.price)
   end
 end
--- ruby/vending_machine/vendor_test.rb
+++ ruby/vending_machine/vendor_test.rb
@@ -1,14 +1,49 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
+require_relative 'suica'
 require_relative 'vendor'
 
 class VendorTest < Minitest::Test
   def setup
     @vendor = Vendor.new
+    @suica = Suica.new
   end
 
   def test_get_stock
     assert_equal 5, @vendor.stock
+  end
+
+  def test_can_buy
+    assert_equal true, @vendor.can_buy?(@suica)
+    # falseパターンはbuyで行う
+  end
+
+  def test_buy_ok
+    @vendor.buy(@suica)
+    assert_equal 4, @vendor.stock
+  end
+
+  def test_buy_ng_deposit_short
+    @vendor.buy(@suica)
+    @vendor.buy(@suica)
+    @vendor.buy(@suica)
+    e = assert_raises(RuntimeError) do
+      @vendor.buy(@suica)
+    end
+    assert_equal 'チャージ残高か在庫が不足しています', e.message
+  end
+
+  def test_buy_ng_stock_short
+    @suica.charge(1000)
+    @vendor.buy(@suica)
+    @vendor.buy(@suica)
+    @vendor.buy(@suica)
+    @vendor.buy(@suica)
+    @vendor.buy(@suica)
+    e = assert_raises(RuntimeError) do
+      @vendor.buy(@suica)
+    end
+    assert_equal 'チャージ残高か在庫が不足しています', e.message
   end
 end
